0f46d15d827c824dc398ae165f2dd00b0c658286
Shifteon <bpwyatt04@gmail.com>
2022-03-10 10:30:54 -0700

    addded a TODO

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
+// TODO: uncomment this stuff
 // const swaggerDocument = require('../swaggerDesign.json');
 router.use('/api-docs', swaggerUi.serve);
 // router.get('/api-docs', swaggerUi.setup(swaggerDocument));
