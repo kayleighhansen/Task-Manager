4132dcdcadbdd01d047875c27960eb2bfb6f30de
rachelblock2 <36903263+rachelblock2@users.noreply.github.com>
2022-03-17 17:53:05 -0600

    Added task routes, moved root routes to svr file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const router = express.Router();
+// Root routes put into server.js file, to match the swagger documentation path for each route,
+// no prefix routes before login, signup, task, etc. 
 
-router.use('/', require('./swagger'));
-router.use('/auth', require('./auth'));
+// const express = require('express');
+// const router = express.Router();
 
-module.exports = router;
+// router.use('/', require('./swagger'));
+// router.use('/user', require('./auth'));
+// router.use('/auth', require('./auth'));
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+// putting routes here in instead of the index file
+const swagger = require('./routes/swagger');
+const taskRoutes = require('./routes/task');
+const authRoutes = require('./routes/auth');
+// const companyRoutes = require('./routes/company');
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -20,9 +26,11 @@ app.use(bodyParser.json())
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       next();
    })
-   // auth routes
-   .use('/auth', require('./routes/auth'))
-   .use('/swagger', require('./routes/swagger'));
+   // putting routes here in instead of the index file
+   .use('/api-docs', swagger)
+   .use('/user', authRoutes)
+   .use('/tasks', taskRoutes);
+   // .use('/company', companyRoutes)
 
 
 // mongoose db connection
