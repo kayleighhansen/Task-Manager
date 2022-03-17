4955abbda048090c53f44bbf2818b4f6f1d27afb
dunntooni <dun15014@byui.edu>
2022-03-17 10:33:34 -0600

    Changed nodemon to node

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 // TODO: uncomment this stuff
-// const swaggerDocument = require('../swaggerDesign.json');
+const swaggerDocument = require('../project_management-Library-1.0.0-resolved.json');
 router.use('/api-docs', swaggerUi.serve);
-// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
