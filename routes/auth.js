0f81ff577f624bd08e565066f9449119c9851be7
rachelblock2 <36903263+rachelblock2@users.noreply.github.com>
2022-03-24 18:17:05 -0600

    I put back the validation for signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,34 +1,32 @@
 const express = require('express');
 const { body } = require('express-validator');
+const isAuth = require('../middleware/isAuth');
 const router = express.Router();
 
 const authController = require('../controllers/auth');
 router.put(
    '/signup',
-   // [
-   //    body('email')
-   //       .isEmail()
-   //       .withMessage('Please enter a valid email.')
-   //       .custom((value, { req }) => {
-   //          return User.findOne({ email: value }).then((userDoc) => {
-   //             if (userDoc) {
-   //                return Promise.reject('Email address exists already!');
-   //             }
-   //          });
-   //       })
-   //       .normalizeEmail(),
-   //    body('password').trim().isLength({ min: 5 }),
-   //    body('name').trim().not().isEmpty(),
-   // ],
+   [
+      body('email')
+         .isEmail()
+         .withMessage('Please enter a valid email.')
+         .custom((value, { req }) => {
+            return User.findOne({ email: value }).then((userDoc) => {
+               if (userDoc) {
+                  return Promise.reject('Email address exists already!');
+               }
+            });
+         })
+         .normalizeEmail(),
+      body('password').trim().isLength({ min: 5 }),
+      body('name').trim().not().isEmpty(),
+   ],
+   // isAuth, //This checks for a JWT token, should be used on all other routes like getting tasks/create company etc.
    authController.signup
 );
 
-router.post('/login', authController.login); // TODO: Add all the other User Requests
+router.post('/login', authController.login); // TODO: Add all the other User Request validation
 
-router.post('/logout', authController.logout); // TODO: Add all the other User Requests
-
-// router.get('/info', authController.info);
-
-// router.get('/test', authController.test);
+router.post('/logout', authController.logout); // TODO: Add all the other User Request validation
 
 module.exports = router;
